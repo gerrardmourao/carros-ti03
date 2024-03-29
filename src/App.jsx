a1d362109a7e59981b63174378d1f02b4e103fa3
gerrardmourao <gerardmo2022@gmail.com>
2024-03-28 23:48:49 -0700

    Projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ export default function App() {
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
       <div className="lg:flex max-w-[920px]">
         <Sedans />
-        <Suvs />
+        <Suvs /> 
         <Luxuosos />
       </div>
     </main>
   )
 }
+
