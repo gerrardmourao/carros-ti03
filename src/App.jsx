aa28d24ed9343f3a88f49d0fc4267640ef275cab
gerrardmourao <gerardmo2022@gmail.com>
2024-03-28 22:12:04 -0700

    instalaçao do tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import Sedans from "./components/Sedans"
-import Suvs from "./components/Suvs"
-import Luxury from "./components/Luxury"
-
+import iconSedans from "./assets/icon-sedans.svg"
+import iconSuvs from "./assets/icon-suvs.svg"
+import iconLuxury from "./assets/icon-luxury.svg"
 
 export default function App() {
-  return(
-    <main className = "py-20 px-6 min-h-svh bg-offwhitebg antialiased lg:flex justify-center items-center">
-      <div className="lg:flex max-w-[920px]">
+  return (
+    <main>
+      <sectin>
+        <div>
+          <img src={iconSedans} alt="ícone dos carros sedan" />
+          <h2>Sedans</h2>
+          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+        </div>
 
-        <Sedans />
-        <Suvs />
-        <Luxury />
+        <div>
+          <img src={iconSuvs} alt="ícone dos carros SUV" />
+          <h2>SUVs</h2>
+          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+        </div>
 
-      </div>
+        <div>
+          <img src={iconLuxury} alt="ícone dos carros de luxo" />
+          <h2>Luxuosos</h2>
+          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+        </div>
+      </sectin>
     </main>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { 
+    colors: {
+      'orange': '#e28625',
+      'green': '#006971',
+      'dark-green': '#004140',
+      'offwhite': 'rgba(255, 255, 255, 0.75)',
+      'white': '#fff',
+      'offwhitebg': '#f2f2f2'
+    },
+
+    fontFamily: {
+      'shoulders':'"Big Shoulders Display"',
+      'lexand':'"Lexand Deca"',
+
+      'lexend':'"Lexend Deca"'
+
+    }
+  },
+  plugins: [],
+}
